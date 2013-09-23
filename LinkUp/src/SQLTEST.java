6c5f5bcb74e64e93aa1968e25b59aaa5bfab5f84
Michelle <mmoore9@ycp.edu>
2013-09-23 12:07:45 -0400

    mmoore 12:07 test

--- LinkUp/src/SQLTEST.java
+++ LinkUp/src/SQLTEST.java
@@ -6,7 +6,8 @@ public class SQLTEST {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+				//michelle testing 1 2 3
+		System.out.println("CHECK MMOORE TEST");
 	}
 
 }
