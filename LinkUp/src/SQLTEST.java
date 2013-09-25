95caf1b369ca9eb95d26cf12a6a0c183f32789ef
Anthony Cummins <ajoshuacummins1@gmail.com>
2013-09-25 15:25:43 -0400

    Added Code to SQLTEST
    I think it's close but I'm getting an "Access Denied" error.
    I'll have to check to make sure the credentials are correct.

--- LinkUp/src/SQLTEST.java
+++ LinkUp/src/SQLTEST.java
@@ -1,16 +1,38 @@
 import java.sql.*;
-import com.mysql.jdbc.*;
 
 
 public class SQLTEST {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-				//michelle testing 1 2 3
-				//kfelton testing 1 2 3
-		System.out.println("CHECK MMOORE TEST");
-		System.out.println("CHECK KFELTON TEST");
-		System.out.println("CHECK ACUMMINS TEST");
+	public static void main(String[] args) throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+        java.sql.Connection con = null;
+        java.sql.PreparedStatement stmt = null;
+        String user = "ajcummins";
+        String pass = "linkup";
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://172.31.5.42:3306/linkup", user, pass);
+            stmt = con.prepareStatement("INSERT INTO 'linkup'.'account_registration'(user_id,first_name,last_name) VALUES (?,?,?)");
+            stmt.setString(1, "999");
+            stmt.setString(2, "Clyde");
+            stmt.setString(3, "Evans");
+            stmt.executeUpdate();
+            con.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (stmt != null) {
+                try {
+                   stmt.close();
+                } catch (SQLException ex) {
+                }
+            }
+            if (con != null) {
+                try {
+                   con.close();                    
+                } catch (SQLException ex) {
+                }
+            }
+        }
 	}
 
 }
