94413429d4c0be4e805bce09d161f74f931600d3
kerry <kfelton@ycp.edu>
2013-09-26 14:38:07 -0500

    profile

--- LinkUp/src/data/Profile.java
+++ LinkUp/src/data/Profile.java
@@ -1,5 +1,73 @@
 package data;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Profile {
 
-}
+	public Profile() {
+		// TODO Auto-generated constructor stub
+		String likes, dislikes, sex;
+	}
+
+
+
+		
+		public String getLikes(){
+			String likes = null;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				likes = reader.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return likes;
+			
+			
+		}
+		
+		public String getDislikes(){
+			String dislikes = null;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				dislikes = reader.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return dislikes;
+			
+			
+			
+			
+		}
+		
+		public String getSex(){
+			String sex = null;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				sex = reader.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if(sex.equalsIgnoreCase("M") || sex.equalsIgnoreCase("F")){
+				
+			}else{
+				System.out.print("Try again. Enter M for male or F for female");
+				getSex();
+			}
+			
+			return sex;
+			
+			
+			
+		}
+		
+
+	}
+
+
