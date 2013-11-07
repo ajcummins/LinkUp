3daab13ddfc581697342d05a77e0975ca6a03f1e
Michelle <mmoore9@ycp.edu>
2013-11-07 17:19:42 -0500

    stuff

--- Controllers/Controllers/src/edu/ycp/cs481/linkup/controller/MessageController.java
+++ Controllers/Controllers/src/edu/ycp/cs481/linkup/controller/MessageController.java
@@ -2,7 +2,6 @@ package edu.ycp.cs481.linkup.controller;
 
 import com.mysql.jdbc.Messages;
 
-import edu.ycp.cs481.linkup.model.Matching;
 import edu.ycp.cs481.linkup.persistence.Database;
 import edu.ycp.cs481.linkup.persistence.PersistenceException;
 
